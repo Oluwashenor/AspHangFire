336d24cfe083e168524191e2e1cb3d55190e8797
Adeshina <adeshiname@gmail.com>
2022-10-06 18:10:56 +0100

    Add project files.

---
+++ Controllers/PersonController.cs
@@ -0,0 +1,39 @@
+﻿using AspHangFire.Data;
+using AspHangFire.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace AspHangFire.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class PersonController : ControllerBase
+    {
+
+        private readonly ApplicationDbContext _context;
+
+        public PersonController(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        [HttpGet]
+        public IActionResult Index()
+        {
+            return Ok("Nice");
+        }
+        [HttpPost]
+        public async Task<IActionResult> Create(Person person )
+        {
+            if (ModelState.IsValid)
+            {
+                await _context.People.AddAsync(person);
+                if(await _context.SaveChangesAsync() > 0)
+                {
+                    return Ok("Person Created,Response");
+                }
+                
+            }
+            return StatusCode(StatusCodes.Status400BadRequest);
+        }
+    }
+}
---
+++ Models/Person.cs
@@ -0,0 +1,9 @@
+﻿namespace AspHangFire.Models
+{
+    public class Person
+    {
+        public Guid Id { get; set; }
+        public string? Name { get; set; }
+        public string? Age { get; set; }
+    }
+}
